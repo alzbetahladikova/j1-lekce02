a105b9b7fa6f035bca1fd4c827ca32a2c231b7a9
alzbetahladikova <beta.strelbova@gmail.com>
2023-02-14 12:06:39 +0100

    domek a kruh

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -8,6 +8,29 @@ public class HlavniProgram {
         zofka = new Turtle();
 
         //TODO tady bude kód
+
+        //vlastní cvičení
+        /* domel
+        for (int i=0; i<4; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(70.7);
+        zofka.turnLeft(90);
+        zofka.move(70.7);
+        zofka.turnLeft(45);
+*/
+        /* kruh
+        for (int i=0; i<365; i++) {
+            zofka.move(1);
+            zofka.turnLeft(1);}
+
+         */
+
+
+
     }
 
     public static void main(String[] args) {

a276b1992c03a0040b5a39514aadf57f7aed85a5
alzbetahladikova <beta.strelbova@gmail.com>
2023-02-14 13:36:50 +0100

    čtverec, trojúhelník,domek a kruh se změnou lokace, několik domků se změnou lokace ručně, několik domků se změnou lokace automaticky, prasátko

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -102,6 +102,41 @@ for (int a=0;a<5;a++) {
 
 }
 
+// prasatko
+        zofka.setLocation(500, 500);
+zofka.turnLeft(90);
+
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(70.7);
+        zofka.turnLeft(90);
+        zofka.move(70.7);
+        zofka.turnLeft(45);
+        zofka.move(20);
+        double predniNohyX =zofka.getX();
+        double predniNohyY= zofka.getY();
+        zofka.turnRight(120);
+        zofka.move(40);
+        zofka.setLocation(predniNohyX,predniNohyY);
+        zofka.turnLeft(60);
+       zofka.move(40);
+       zofka.setLocation(predniNohyX,predniNohyY);
+       zofka.turnLeft(60);
+        zofka.move(70);
+        double zadniNohyX =zofka.getX();
+        double zadniNohyY= zofka.getY();
+        zofka.turnRight(120);
+        zofka.move(40);
+        zofka.setLocation(zadniNohyX,zadniNohyY);
+        zofka.turnLeft(60);
+        zofka.move(40);
+
+
+
     }
 
     public static void main(String[] args) {
